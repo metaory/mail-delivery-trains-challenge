6a1aa88b0adeb61db9c89626b813f2509596f641
metaory <metaory@gmail.com>
2023-07-19 10:56:09 +0800

    annotaited the code

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ import { readFileSync } from "node:fs";
 
 console.clear();
 
+// Debug logger tag function
 function log(tpl, ...vars) {
   for (const [i, key] of tpl.entries()) {
     if (!key) continue;
-    process.stdout.write(`${key}${C.red(vars[i])}`);
+    process.stdout.write(`${key}${C.red.bold(vars[i])}`);
   }
   process.stdout.write("\n");
 }
 
+// Load up the input.json
 const input = JSON.parse(readFileSync("./input.json", { encoding: "utf8" }));
 console.log("input:", input);
-
-// ************************************************************************** //
 /* INPUT:
   {
     "stations": ["A", "B", "C", "D"],
@@ -24,6 +24,9 @@ console.log("input:", input);
   }
 */
 
+// ************************ //
+
+// Destructure what we need from input
 const {
   // stations, // NOTE: we dont need this!
   edges,
@@ -33,6 +36,7 @@ const {
 
 // NOTE: we'll also ignore weight constraint entirely
 
+// Reduce input to prepare connection map and distances
 const { connections, distances } = edges.reduce(
   (acc, cur) => {
     const [, from, to, distance] = cur.split(",");
@@ -54,63 +58,88 @@ console.log(connections);
 console.log(distances);
 // { 'A-B': 30, 'B-A': 30, 'B-C': 10, 'C-B': 10, 'C-D': 40, 'D-C': 40 }
 
+// Destructure train name and its initial language
 const [train, , initialLocation] = TRAIN.split(","); // NOTE: ignoring capacity
-// log("initialLocation:", initialLocation);
+`initialLocation: ${initialLocation}`;
 
+// Out global state
 const moves = [];
 let time = 0;
-let trainLocation = initialLocation;
+let current = initialLocation;
 
+// Return the immediate next possible move
 const getNext = (from, to) => {
+  // Possible next moves
   let [next, alt] = connections[from];
 
+  // The alteranative is the destination
   if (alt === to) {
     next = alt;
   }
 
+  // Dont know which direction to go
   if (alt && alt !== to) {
     // TODO: greedy traverse both end
+    // TODO: figure out which direction to go!!!
     console.log("AT JUNCTION!");
   }
-
-  // TODO: decide which direction to go!!!
 
   return next;
 };
 
-function moveTrain(from, to, pkg = null) {
-  // console.log(`move train from ${C.red(from)} to ${C.red(to)} with ${pkg}`);
-  log`move train from ${from} to ${to} with ${pkg}`;
+// Move the train between two points ( A -> F )
+function moveTrain(to, pkg = null) {
+  log`move train from ${current} to ${to} with ${pkg}`;
 
-  let current = from;
-  let next = getNext(from, to);
-  let DEBUG_ESCAPE_HATCH = 0; // NOTE: temporary for debug purposes
+  // Our local state
+  let next = getNext(current, to);
 
+  // NOTE: Temporary for debug purposes
+  let DEBUG_ESCAPE_HATCH = 0;
+
+  // Move untill we've reached destinations
   while (current !== to) {
     DEBUG_ESCAPE_HATCH++;
+
     log`next: ${next}`;
+
+    // Append the output
     moves.push(`W=${time}, T=${train}, N1=${current}, N2=${next}, P2=[${pkg}]`);
+
+    // Update local state
     time += distances[`${current}-${next}`];
     current = next;
-    trainLocation = current;
     next = getNext(current, to);
-    log`trainLocation: ${trainLocation}`;
-    if (DEBUG_ESCAPE_HATCH > 10) process.exit(); // NOTE: temporary for debug
+
+    log`current: ${current}`;
+
+    // NOTE: Temporary for debug purposes
+    if (DEBUG_ESCAPE_HATCH > 10) process.exit();
   }
 }
+
 // XXX: DEBUG:
 // moveTrain("A", "B");
 // moveTrain("A", "C");
-moveTrain("B", "A");
-// moveTrain("A", "D"); // TODO: <<<
+// moveTrain("B", "A");
+// moveTrain("A", "D");
+// ^^^^^^^^^^^^^^^^^^^
 
-// for (const delivery of deliveries) {
-//   const [pkg, weight, src, dst] = delivery.split(",");
-//   // move train to delivery pickup location
-//   // moveTrain(trainLocation, src);
-//
-//   // move train to destination
-//   moveTrain(trainLocation, dst, pkg);
-// }
+// Iterate over deliveries
+for (const delivery of deliveries) {
+  // Destructure delivery detail
+  const [pkg, weight, src, dst] = delivery.split(",");
 
-console.log("moves:", moves);
+  // Move train to delivery pickup location
+  moveTrain(src);
+
+  // Move train to destination
+  moveTrain(dst, pkg);
+}
+
+console.log("------------------------");
+console.log("-RESULT-----------------");
+console.log("------------------------");
+
+// Output the result
+console.log(moves);
